df7efd2c25c1af7e6df9bd4a0db1ad7e14751e76
Raj Kumar C G <rajkumarcg18@gmail.com>
2025-07-11 22:44:16 +0530

    created footer component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import Header from "./components/header";
+import Footer from "./components/footer";
+import { Container } from "react-bootstrap";
 
 
 function App() {
   return (
     <>
     <Header/>
+    <Container>
+      <main>
+        
+      </main>
+    </Container>
+    <Footer/>
     </>
   );
 }
---
+++ src/components/footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {Row, Col} from "react-bootstrap"
+
+const Footer =() => {
+
+    const currentYear = new Date().getFullYear()
+    return(
+        <footer>
+            <Row className="text-center">
+                <Col>
+                      VTU &copy;{currentYear}
+                </Col>
+            </Row>
+        </footer>
+    )
+}
+export default Footer
