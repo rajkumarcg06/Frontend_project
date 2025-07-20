281f76dc1100ac5146323812865bc7e6298c132c
Raj Kumar C G <rajkumarcg18@gmail.com>
2025-07-21 01:44:06 +0530

    updating map to project

--- src/screens/VTUContacts.js
+++ src/screens/VTUContacts.js
@@ -97,6 +97,7 @@ const contactData = [
 
 const VTUContacts = () => {
   return (
+    <>
     <Container className="my-4">
       <Row >
         {contactData.map((contact) => (
@@ -118,7 +119,18 @@ const VTUContacts = () => {
           </Col>
         ))}
       </Row>
+      
     </Container>
+    <Row>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3839.4960612344908!2d74.4634341!3d15.7777762!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bbf6589aaaaaaa9%3A0xce6d455b3335886!2sVisvesvaraya%20Technological%20University%20(VTU)!5e0!3m2!1sen!2sin!4v1753041993834!5m2!1sen!2sin"
+        width="100%"
+        height="450"
+        style={{ border: 0 }}
+        allowFullScreen=""
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"/>
+     </Row>
+     </>
   );
 };
 
