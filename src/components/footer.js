473cdd796129c4c16cec36c3518cb764da82e042
Raj Kumar C G <rajkumarcg18@gmail.com>
2025-07-21 20:30:41 +0530

    adding social meadia tools

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {Row, Col} from "react-bootstrap"
-import { BsFacebook,BsLinkedin,BsChat, BsTwitter } from 'react-icons/bs';
+import { BsFacebook,BsLinkedin,BsChatRight, BsTwitter,BsWhatsapp } from 'react-icons/bs';
 
 const Footer =() => {
 
@@ -9,23 +9,25 @@ const Footer =() => {
         <footer >
             <Row className="text-center" >
                 <Col>
-                      VTU &copy;{currentYear}
+                      <b>VTU &copy;{currentYear}</b>
                 </Col>
             </Row>
-            <div style={{marginLeft:'55rem',gap:"2rem"}}>
-                <Row style={{gap:'2rem', marginTop:'2rem'}}>
-                <a href="https://www.facebook.com/vtuinfo/" >
-                    <BsFacebook size={24} color="#4267B2" /></a>
-                <a href="https://www.linkedin.com/school/visvesvaraya-technological-university/?originalSubdomain=in">
-                    <BsLinkedin size={24} color="#4267B2" /></a>
+            <Row className="text-center my-4"  >
+                <Col >
+                      <a href="https://www.facebook.com/vtuinfo/" style={{marginRight:'2rem'}} >
+                    <BsFacebook size={35} color="#4267B2" /></a>
+                <a href="https://www.linkedin.com/school/visvesvaraya-technological-university/?originalSubdomain=in"style={{marginRight:'2rem'}}>
+                    <BsLinkedin size={35} color="#4267B2" /></a>
                 <a href="https://x.com/vtuinfo?lang=en">
-                    <BsTwitter size={24} color="#4267B2" /></a>
+                    <BsTwitter size={35} color="#4267B2" /></a>
                 <a href="https://www.justdial.com/Bangalore/
                 Visveshwaraya-Technological-University-Regional-Office-Near-BDA-Complex-Annapoorneswari-Nagar
-                /080P5602086_BZDET/reviews"  style={{marginLeft:'48rem'}}>
-                    <BsChat size={24} color="#4267B2" /></a>
-                </Row>
-            </div>
+                /080P5602086_BZDET/reviews" style={{float:'right',marginTop:'-8rem',marginRight:'2rem'}}>
+                    <BsChatRight size={40} color="#050505ff" /></a>
+                <a href="https://chat.whatsapp.com/EkV0hLSw82Y6PZuqu9ldy3" style={{float:'right',marginTop:'-12rem',marginRight:'2rem'}}>
+                    <BsWhatsapp size={40} color="#13950fff" /></a>
+                </Col>
+            </Row>
         </footer>
     )
 }
